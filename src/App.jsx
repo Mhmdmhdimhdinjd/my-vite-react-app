b5a55f5ad1b27c4d349f53a58615bfbfc344bf63
mhmd mhdi <mhdimhdin5@gmail.com>
2024-12-23 12:08:10 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <p>Welcome to my Vite React App!</p>
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
